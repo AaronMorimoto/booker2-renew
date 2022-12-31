84ca2006c030c36c5b0f80f6fc2de75ea74cffed
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-31 02:53:07 +0000

    [follow Add2]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   devise_for :users
   root :to =>"homes#top"
   get "home/about" => "homes#about"
@@ -8,6 +6,9 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
-  
+  resources :users, only: [:index,:show,:edit,:update] do
+    resources :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
