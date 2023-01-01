91ab19597dc70d6bf6af93175de00c58b389ca07
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-01-01 11:05:21 +0000

    [searche Start]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,19 @@
 class RelationshipsController < ApplicationController
   # フォローするとき
   def create
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
+    user = User.find(params[:user_id])
+    current_user.follow(user)
+		redirect_to request.referer
+    #current_user.follow(params[:user_id])
+    #redirect_to request.referer
   end
   # フォロー外すとき
   def destroy
-    current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
+		redirect_to request.referer
+    #current_user.unfollow(params[:user_id])
+    #redirect_to request.referer
   end
   # フォロー一覧
   def followings
