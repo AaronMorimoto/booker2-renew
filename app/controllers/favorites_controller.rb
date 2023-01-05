546f23f3581a3e4b0bff3233cb581c3822908e96
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-01-05 10:39:30 +0000

    [fav-com renewed]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,25 @@
 class FavoritesController < ApplicationController
   
   def create
-    @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
-    @book_favorite.save
+    #@book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
+    #@book_favorite.save
     #redirect_to books_path(params[:book_id])
-    render 'create'
+    #render 'create'
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.save
+    render 'replace_btn'
   end
   
   def destroy
-    @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
-    @book_favorite.destroy
+    #@book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
+    #@book_favorite.destroy
     #redirect_to books_path(params[:book_id])
-    render 'destroy'
+    #render 'destroy'
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: book.id)
+    @favorite.destroy
+    render 'replace_btn'
   end
   
 end
